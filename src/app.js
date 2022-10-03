69b6a332ca902c430f7bc7fbcda9919f57a542ac
Borys <borysm@gmail.com>
2022-10-03 18:13:13 +0300

    init commit

---
+++ src/app.js
@@ -0,0 +1,34 @@
+import { Application, Request, Response, NextFunction } from "express";
+import type { ErrorRequestHandler } from "express";
+
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const notesRouter = require('./routes/notes');
+
+const app:Application = express();
+
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use('/notes', notesRouter);
+
+app.use((req:Request, res:Response) => {
+  res.status(404).json({ message: 'Not found' })
+});
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next:NextFunction) => { 
+  const { status = 500, message = 'Server error' } = err
+  res.status(status).json({ message: err.message })
+};
+
+app.use(errorHandler);
+
+module.exports = app;
---
+++ src/repositories/schemas/notesModel.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { NoteIface } from "../../helpers/noteIface";
+
+class NotesModel{
+
+    checkNote(){
+
+    }
+   
+    checkUid(){
+
+    }
+
+    checkDate(){
+
+    }
+};
+
+module.exports = { NotesModel };
